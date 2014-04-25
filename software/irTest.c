9c94b2c8cd9520778cb06b9ccc00c874d88bcb52
John Daniel Sutton <jdsutton@calpoly.edu>
2014-04-25 11:03:10 -0700

    Add IR interrupt code
    Added code to test pin interrupts with the IR sensor.
    Added a makefile which links in code from the Arduino standard
    libraries.

---
+++ software/irTest.c
@@ -0,0 +1,161 @@
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\Arduino.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define LED_PIN 7
+#define INPUT_PIN 2
+
+volatile int state = 0;
+
+//initialize timer 0
+void lab4_initialize_timer0(void) {
+    TCCR0B |= (1 << CS02);   //set timer 0 to increment at 62.5KHz
+}
+
+//Initialize Timer 1
+void initialize_timer(void) {
+    TCCR1B = 2;                  //increment timer at 2MHz
+    TIMSK1 |= (1 << OCIE1A); //enable output compare match
+    sei();                   //enable global interrupts
+}
+
+
+//Interrupt Funciton
+ISR(TIMER1_COMPA_vect){
+
+    //OCR1A +=speed_1;
+
+}
+
+
+//Check Falling Edge
+char checkForFallingEdge (int timeInMilsec)
+{
+    TCNT0 = 0;
+    //int timeInTicks = 0;
+    //timeInTicks = ((63) * timeInTicks);
+
+    char previousRead = 0;
+    char currentRead;
+    //int fallOccuredInTime = 0;
+
+    while (TCNT0 < timeInMilsec)
+    {
+        currentRead = PIND & (1 << INPUT_PIN);
+        // currentRead = (currentRead >> 2);
+
+        if (!currentRead && previousRead)
+        {
+            //A Fall Occured
+            return(1);
+        }
+        previousRead = currentRead;
+    }
+    return(0);
+}
+
+//Read Data Bit
+char readDataBit(void)
+{
+    if (checkForFallingEdge(75))
+    {
+        //Fall Detected. Continue.
+
+        if (!(checkForFallingEdge(57)))
+        {
+            //Fall Not Detected. Continue.
+
+
+            if ((PIND & (1 << INPUT_PIN)) != 0)
+            {
+                return(0);
+            }
+            else
+            {
+                return(1);
+            }
+        }
+        else
+        {
+            //Fall  Detected. Abort.
+            return(2);
+        }
+    }
+    else
+    {
+        //Fall Not Detected
+        return(2);
+    }
+}
+
+//Wait for Starting Bit
+void waitForStartBit(void)
+{
+    char previousRead = 0;
+    char currentRead;
+
+    while(1)
+    {
+        currentRead = PIND & (1 << INPUT_PIN);
+        //currentRead = (currentRead >> 2);
+
+        if (!currentRead && previousRead)
+        {
+            //Falling Edge Detected
+            if (!checkForFallingEdge(157))
+            {
+                //Fall Did Not Occur. Continue.
+                break;
+            }
+        }
+        previousRead = currentRead;
+    }
+}
+
+void blink(void)
+{
+    state = !state;
+    if (state)
+    {
+        PORTD |= (1 << LED_PIN);
+    }
+    else
+    {
+        PORTD &= !(1 << LED_PIN);
+    }
+}
+
+//Main
+int main(void)
+{
+    char dataBitArray[7];
+
+    //Initialize Timers
+    //lab4_initialize_timer0();
+    //initialize_timer(); // Start Timer
+    //OCR1A = 2000;
+
+    //Setup pins for input and output
+    DDRD |= (1 << LED_PIN); //Sets pin 1 to output
+    sei();
+    attachInterrupt(0, blink, CHANGE);
+
+    while(1)
+    {
+
+    }
+
+    //PORTB |= (1<< 0);
+    //if (PINB & (1 << 0)))
+
+    return 0;
+}
+
+
+
+
+
+
+
+
