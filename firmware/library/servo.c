ffbf2992b5d2216be5f271f1f6de8e7c70eca925
John Seng <jseng@calpoly.edu>
2015-01-25 22:57:25 -0800

    updated servo library

--- firmware/library/servo.c
+++ firmware/library/servo.c
@@ -6,31 +6,24 @@
 u08 servo_state=0;
 u16 servo_high_time[4];
 u16 servo_low_time[4];
-u16 count=0;
 
 ISR(TIMER1_COMPA_vect) {
-   /*
    if (servo_state & 1) {
       OCR1A += servo_low_time[servo_state >> 1];
-      //PORTC &= ~_BV(servo_state >> 1);
+      PORTC &= ~_BV(servo_state >> 1);
    } else {
       OCR1A += servo_high_time[servo_state >> 1];
-      //PORTC |= _BV(servo_state >> 1);
+      PORTC |= _BV(servo_state >> 1);
    }
 
    servo_state += 1;
    servo_state &= 7;
-   */
-   OCR1A += 1000;
-   count++;
-   count = count % 65000;
 }
 
 void set_position(u08 servo_num, u08 position) {
-//   while((servo_state >> 1) == servo_num) {}
-
-   servo_high_time[servo_num] = 2000 + 10 * position;
-   servo_low_time[servo_num] = 10000 - servo_high_time[servo_num];
+   //position from 0 to 250
+   servo_high_time[servo_num] = 2500 + 10 * position;
+   servo_low_time[servo_num] = 12500 - servo_high_time[servo_num];
 }
 
 void init_servo(void) {
@@ -42,7 +35,7 @@ void init_servo(void) {
 
    DDRC |= _BV(SERVO0_PIN) | _BV(SERVO1_PIN) | _BV(SERVO2_PIN) | _BV(SERVO3_PIN);
    TCCR1B = 0;
-   TCCR1B |= _BV(CS12);
+   TCCR1B |= _BV(CS11);
    TIMSK1 |= _BV(OCIE1A);
    sei();
 }
