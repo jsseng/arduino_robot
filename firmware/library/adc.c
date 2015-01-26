e4fef69d4b705125b4688ad0c7072e3624a0837c
John Seng <jseng@calpoly.edu>
2015-01-25 22:57:53 -0800

    updated analog conversion functions

--- firmware/library/adc.c
+++ firmware/library/adc.c
@@ -4,10 +4,19 @@
 #include <util/delay.h>
 
 void init_adc(void) {
+   ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+   ADMUX |= _BV(ADLAR) | _BV(REFS0);
    return;
 }
 
-ISR(ADC_vect) {
-}
+u08 analog(u08 num) {
+   ADMUX &= 0xF8;
+   ADMUX |= num;
 
+   ADCSRA |= _BV(ADSC);
+
+   while (ADCSRA & _BV(ADSC)) {}
+
+   return ADCH;
+}
 
