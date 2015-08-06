daa76e7654d2bb592cd42b72663419e04c6b5c52
John Seng <jsseng@gmail.com>
2015-08-06 09:07:29 -0700

    stop the ModemManager service on linux

--- firmware/testing/setup.sh
+++ firmware/testing/setup.sh
@@ -6,5 +6,10 @@ echo "ACTION==\"add\", ATTRS{idVendor}==\"04b4\", ATTRS{idProduct}==\"0003\", SY
 udevadm control --reload-rules
 udevadm trigger
 
+sudo mv /usr/share/dbus-1/system-services/org.freedesktop.ModemManager1.service /usr/share/dbus-1/system-services/org.freedesktop.ModemManager1.service.disabled
 
+sudo sed -i '/exit 0/c\' /etc/rc.local
+sudo sed -i '/killall/c\' /etc/rc.local
+echo "killall ModemManager" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
 
