81eeb1b48085ed63b7cf0afe2a5e178e0c5f3f56
John Seng <jseng@John-Sengs-iMac.local>
2015-08-07 12:58:31 -0700

    added program file

---
+++ firmware/workshop/program.c
@@ -0,0 +1,20 @@
+#include "globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include "USI_TWI_Master.h"
+#include <avr/interrupt.h>
+
+int main(void) {
+   init();
+
+   /* Type in your program after this line */
+
+
+
+
+
+
+   /* Do not type anything after this line */
+
+   return 0;
+}
