90d2fe33098cdb46a24689366f54ed6b307f0ec5
John Seng <jseng@jseng.csc.calpoly.edu>
2015-09-22 13:02:27 -0700

    simplified starter code

--- firmware/workshop/program.c
+++ firmware/workshop/program.c
@@ -1,20 +1,12 @@
 #include "globals.h"
 #include <util/delay.h>
 #include <avr/io.h>
-#include "USI_TWI_Master.h"
 #include <avr/interrupt.h>
 
 int main(void) {
    init();
 
-   /* Type in your program after this line */
-
-
-
-
-
-
-   /* Do not type anything after this line */
+   //print_string("test");
 
    return 0;
 }
